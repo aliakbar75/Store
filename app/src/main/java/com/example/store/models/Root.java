fcba493f22063456eb547daf5bd68ee8d8844b7f
moein <moinracg56@gmail.com>
2019-02-18 18:22:30 +0330

    models added. root model added.

---
+++ app/src/main/java/com/example/store/models/Root.java
@@ -0,0 +1,18 @@
+package com.example.store.models;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class Root {
+    @SerializedName("products")
+    private List<Product> mProducts;
+
+    public Root(List<Product> products) {
+        mProducts = products;
+    }
+
+    public List<Product> getProducts() {
+        return mProducts;
+    }
+}
